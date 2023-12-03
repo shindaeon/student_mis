68d7200ed89602702f2dfb5b44d0dfdc8401f9de
Dion Mark Corales <navybl4456@gmail.com>
2023-12-03 14:09:02 +0800

    Restructured Print (Code Functions)

--- frmMain.vb
+++ frmMain.vb
@@ -72,68 +72,19 @@ Public Class frmMain
     End Sub
 
     Private Sub cmdPrint_Click(sender As Object, e As EventArgs) Handles cmdPrint.Click
-
         PrintPreviewDialog1.Document = PrintDocument1
-        PrintDocument1.DefaultPageSettings.PaperSize = New System.Drawing.Printing.PaperSize("Paper Size Name", 936, 612)
+        PrintDocument1.DefaultPageSettings.PaperSize = New System.Drawing.Printing.PaperSize("Letter (Landscape 150dpi)", 1648, 1276)
         PrintPreviewDialog1.WindowState = FormWindowState.Maximized
+        PrintPreviewDialog1.PrintPreviewControl.Zoom = 0.75
         PrintPreviewDialog1.ShowDialog()
+
     End Sub
 
     Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
-        Dim format As New StringFormat
-        format.Alignment = StringAlignment.Center
-        e.Graphics.DrawString("List of Students", New Font("Arial", 20, FontStyle.Bold),
-                              Brushes.Black, New Point(468, 20), format)
-        Dim fmt As StringFormat = New StringFormat(StringFormatFlags.LineLimit)
-        fmt.LineAlignment = StringAlignment.Center
-        fmt.Trimming = StringTrimming.EllipsisCharacter
-
-        Dim y As Integer = 100
-        Dim x As Integer = 150
-        Dim h As Integer = 0
-        Dim rc As Rectangle
-        Dim row As DataGridViewRow
-
-        If newPage Then
-            row = DataGridView1.Rows(mRow)
-            x = 1
-            For Each cell As DataGridViewCell In row.Cells
-                If cell.Visible Then
-                    rc = New Rectangle(x, y, cell.Size.Width, cell.Size.Height)
-                    e.Graphics.FillRectangle(Brushes.LightGray, rc)
-                    e.Graphics.DrawRectangle(Pens.Black, rc)
-
-                    e.Graphics.DrawString(DataGridView1.Columns(cell.ColumnIndex).HeaderText, DataGridView1.Font, Brushes.Black, rc, fmt)
-                    x += rc.Width
-                    h = Math.Max(h, rc.Height)
-                End If
-            Next
-            y += h
-        End If
-
-        newPage = False
-        Dim displayNow As Integer
-        For displayNow = mRow To DataGridView1.RowCount - 1
-            row = DataGridView1.Rows(displayNow)
-            x = 150
-            h = 0
-
-            For Each cell As DataGridViewCell In row.Cells
-                If cell.Visible Then
-                    rc = New Rectangle(x, y, cell.Size.Width, cell.Size.Height)
-                    e.Graphics.DrawRectangle(Pens.Black, rc)
-
-                    fmt.Alignment = StringAlignment.Near
-                    rc.Offset(10, 0)
-
-                    e.Graphics.DrawString(cell.FormattedValue.ToString(),
-                                          DataGridView1.Font, Brushes.Black, rc, fmt)
-                    x += rc.Width
-                    h = Math.Max(h, rc.Height)
-                End If
-            Next
-            y += h
-        Next
+        Dim imagebmp As New Bitmap(Me.DataGridView1.Width, Me.DataGridView1.Height)
+        e.Graphics.DrawString("List of Students", New Font("Arial", 20, FontStyle.Bold), Brushes.Black, New Point(721, 20))
+        DataGridView1.DrawToBitmap(imagebmp, New Rectangle(0, 0, Me.DataGridView1.Width, Me.DataGridView1.Height))
+        e.Graphics.DrawImage(imagebmp, 20, 70)
     End Sub
 
     Private Sub cmdEdit_Click(sender As Object, e As EventArgs) Handles cmdEdit.Click
@@ -150,7 +101,7 @@ Public Class frmMain
 
     Private Sub cmdDelete_Click(sender As Object, e As EventArgs) Handles cmdDelete.Click
         Dim answer As Integer
-        answer = MsgBox("Do you want to Delete the Record of: " & vbCrLf &
+        answer = MsgBox("DO YOU WANT TO DELETE THE RECORD OF: " & vbCrLf &
                         vbTab & frmEdit.txtEditID.Text & vbCrLf &
                         vbTab & frmEdit.txtEditName.Text & vbCrLf &
                         vbTab & frmEdit.cboEditCollege.Text & vbCrLf &
@@ -166,7 +117,7 @@ Public Class frmMain
                 DBClose()
                 MessageBox.Show("Successfully Deleted Record", "Delete Student", MessageBoxButtons.OK, MessageBoxIcon.Information)
             Else
-                MessageBox.Show("Cancelled", "Cancelled Delete Student", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                MessageBox.Show("Cancelled", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information)
             End If
         Catch ex As Exception
             MsgBox(ex.ToString)
